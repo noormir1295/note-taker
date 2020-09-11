d99dc8f980ac792c6bc2a73f1d3ac93048ec368c
Noor <noormir1295@gmail.com>
2020-09-11 18:00:24 -0400

    adding routes js and starter code for serverjs

---
+++ server.js
@@ -0,0 +1,18 @@
+const express= require("express");
+
+const app=express();
+const PORT= process.env.PORT || 3000
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`))
