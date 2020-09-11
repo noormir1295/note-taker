8b4fb432dab5af3ce7434deec2bd795691647c01
Noor <noormir1295@gmail.com>
2020-09-11 18:25:10 -0400

    finishing html route

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+//Dependencies
+const path =require("path");
+const router= require("express").Router();
+
+//path for notes 
+router.get("/notes",(req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
+router.get("*", (req, res) => {
+res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+module.exports= router
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+//Dependencies 
 const express= require("express");
-
+// const apiRoute= require("./routes/apiRoutes");
+const htmlRoute=require("./routes/htmlRoutes");
 const app=express();
-const PORT= process.env.PORT || 3000
+const PORT= process.env.PORT || 3000;
+
+
+// app.use("/api", apiRoute);
+app.use("/", htmlRoute);
 
 
 
