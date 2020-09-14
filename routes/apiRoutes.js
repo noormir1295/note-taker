c9e15f2cbe52c3079c7773ee7787e0aba375e54b
Noor <noormir1295@gmail.com>
2020-09-14 17:00:26 -0400

    adding apiroute and storejs

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const store = require("../db/store.js");
+
+router.get("/notes", function(req, res){
+    store.getNotes()
+    .then(notes => res.json(notes));
+});
+
+router.post("/notes", function(req, res){
+    store.addNote(req.body)
+    .then(note => res.json(note))
+});
+
+router.delete("/notes/:id", function(req, res) {
+    store.removeNote(req.params.id)
+    .then(() => res.json({ok: true }))
+});
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 //Dependencies 
 const express= require("express");
-// const apiRoute= require("./routes/apiRoutes");
+const apiRoute= require("./routes/apiRoutes");
 const htmlRoute=require("./routes/htmlRoutes");
 const app=express();
-const PORT= process.env.PORT || 3000;
+const PORT= process.env.PORT || 4000;
+
+//
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-// app.use("/api", apiRoute);
+app.use(express.static("public"));
+app.use("/api", apiRoute);
 app.use("/", htmlRoute);
-
-
 
 
 
